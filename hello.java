92b69633309bd6195c1cc0c66725aa36a737beb7
pranathidevaki <pdevakimf@gmail.com>
2026-09-15 12:39:27 +0530

    removed a line to check working of poll scm

--- hello.java
+++ hello.java
@@ -2,6 +2,5 @@ class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Jenkins");
-        System.out.println("Jenkins is an open-source automation server");
     }
 }
